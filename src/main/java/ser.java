3d97a9cd3e079454192a0d903423219254df019b
lmk <1583214829@qq.com>
2019-09-04 13:27:52 +0800

    AI

---
+++ src/main/java/ser.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/s")
+public class ser extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        System.out.println(name);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="s" method="get">
+	<input type="text" name="name"><br>
+	<input type="submit" value="tijiao">
+</form>
+</body>
+</html>
